b91fb0517045aa77436e4a44bce6436b6b1fac40
Stephy <atienostephanie040@gmail.com>
2023-01-24 14:12:09 +0000

    states update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,6 +4,7 @@ from models.state import State
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 
+
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def create_list_states():
     """Retrieves the list of all State objects"""
@@ -21,7 +22,9 @@ def create_list_states():
         new_state.save()
         return jsonify(new_state.to_dict()), 201
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
 def update_delete_states():
     """Updates or Deletes a State"""
     state = storage.get(State, state_id)
