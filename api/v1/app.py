471583f8cf75d1dec1b7a3153979a05265b178f9
Stephy <atienostephanie040@gmail.com>
2023-01-24 11:50:05 +0000

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script to return the status of an API"""
-from api.vi.views import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 from os import getenv
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,23 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = [Amenity, City, Place, Review, State, User]
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     '''This returns a JSON'''
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     '''Returns the number of objects each object has'''
     return jsonify(
